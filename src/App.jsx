d312fb7ab180e775577b73525563015d9fe33970
arun <akrao03213@gmail.com>
2025-04-07 18:12:28 +0530

    started with project e-visa

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Pages/Main_page/Home_Page/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-blue-500'>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
